4875aec92bccab5851b9697aad8ccda3507611be
r.spirichkin <r.spirichkin@qiwi.com>
2019-01-02 13:15:48 +0300

    Initial Commit

---
+++ reminder-shop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  reminder-shop
+//
+//  Created by Roman Spirichkin on 02/01/2019.
+//  Copyright © 2019 perfectware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
